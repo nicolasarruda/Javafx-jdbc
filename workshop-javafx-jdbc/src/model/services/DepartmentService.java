dea67092cab86f65c47596a600c751b7f9b39076
Nicolas <nlarruda@hotmail.com>
2021-08-24 11:28:32 -0300

    Adding database acess

--- workshop-javafx-jdbc/src/model/services/DepartmentService.java
+++ workshop-javafx-jdbc/src/model/services/DepartmentService.java
@@ -1,8 +1,9 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
@@ -10,11 +11,9 @@ public class DepartmentService {
 	// "MOCKar" quando vc retorna os dados de mentirinha. Não é ainda os dados
 	// do banco de dados.
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll(){
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Eletronics"));
-		return list;
+		return dao.findAll();
 	}
 }
